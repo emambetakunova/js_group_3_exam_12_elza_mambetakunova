b496ea1af6330dabf1d241493927bec6ed9d423b
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-25 11:22:48 +0600

    added role for author

---
+++ exam12_api/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+const nanoid = require('nanoid');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema({
+    username: {
+        type: String, required: true, unique: true
+    },
+    password: {
+        type: String, required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        default: 'user',
+        enum: ['user', 'author']
+    },
+    token: {
+        type: String, required: true
+    },
+    avatarImage: String,
+    displayName: {
+        type: String,
+        required: true
+    },
+    facebookId: {
+        type: String
+    }
+});
+
+UserSchema.methods.checkPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function() {
+    this.token = nanoid();
+};
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+
+    next();
+});
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
