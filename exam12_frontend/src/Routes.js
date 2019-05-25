696a1a80a6d0ba8cb4ee01fa4f412fd46c2ea054
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-05-25 17:57:31 +0600

    add AuthorList

--- exam12_frontend/src/Routes.js
+++ exam12_frontend/src/Routes.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
-import ArtistsBuilder from "./containers/ArtistsBuilder/ArtistsBuilder";
-import Artist from "./containers/Artist/Artist";
-import Album from "./containers/Album/Album";
-import TrackHistory from "./containers/TrackHistory/TrackHistory";
-import NewArtist from "./containers/NewArtist/NewArtist";
-import NewAlbum from "./containers/NewAlbum/NewAlbum";
-import NewTrack from "./containers/NewTrack/NewTrack";
+import Gallery from "./containers/Gallery/Gallery";
+import AuthorList from "./containers/AuthorList/AuthorList";
+import NewGallery from "./containers/NewGallery/NewGallery";
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
 
 const Routes = () => {
     return (
         <Switch>
-            <Route path="/" exact component={ArtistsBuilder}/>
-            <Route path="/trackHistory" exact component={TrackHistory}/>
-            <Route path="/artists/new" exact component={NewArtist}/>
-            <Route path="/albums/new" exact component={NewAlbum}/>
-            <Route path="/tracks/new" exact component={NewTrack}/>
+            <Route path="/" exact component={Gallery}/>
+            <Route path="/gallery/new" exact component={NewGallery}/>
             <Route path="/register" exact component={Register}/>
             <Route path="/login" exact component={Login}/>
-            <Route path="/albums/:id" component={Album}/>
-            <Route path="/artists/:id" component={Artist}/>
+            <Route path="/gallery/?author=:id" component={AuthorList}/>
         </Switch>
     );
 };
